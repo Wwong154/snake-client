fbc28d00dcf407df538e244931db2339ec7213cc
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-12-01 19:16:20 +0000

    move input related function to input.js

--- play.js
+++ play.js
@@ -1,17 +1,7 @@
-// play.js
 const { connect } = require('./client');
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = process.stdin.on('data', (key) => {
-  if (key === '\u0003') process.exit();
-});
+const { setupInput } = require('./input')
 
 console.log('Connecting ...');
-setupInput();
-handleUserInput;
 connect();
+
+setupInput();
